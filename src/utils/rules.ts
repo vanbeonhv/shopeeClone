f2a76e936a6bac8e08991dec263a94f402370b08
marc <nguyenvannhv26@gmail.com>
2023-03-20 21:13:13 +0700

    Add Input component

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,5 +1,6 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import { UseFormGetValues, type RegisterOptions } from 'react-hook-form';
-
+import * as yup from 'yup';
 /* eslint-disable prettier/prettier */
 type Rules = { [key in 'email' | 'password' | 'confirm_password']?: RegisterOptions }
 export const getRules = (getValues?: UseFormGetValues<any>): Rules => ({
@@ -53,3 +54,27 @@ export const getRules = (getValues?: UseFormGetValues<any>): Rules => ({
       : undefined
   }
 });
+
+export const schema = yup.object({
+  email: yup
+    .string()
+    .required('Email là bắt buộc')
+    .email('Email không đúng định dạng')
+    .min(6, 'Độ dài từ 6-160 ký tự')
+    .max(160, 'Độ dài từ 6-160 ký tự'),
+  password: yup
+    .string()
+    .required('Password là bắt buộc')
+    .min(6, 'Độ dài từ 6-160 ký tự')
+    .max(160, 'Độ dài từ 6-160 ký tự'),
+  confirm_password: yup
+    .string()
+    .required('Nhập lại password là bắt buộc')
+    .min(6, 'Độ dài từ 6-160 ký tự')
+    .max(160, 'Độ dài từ 6-160 ký tự')
+    .oneOf([yup.ref('password')], 'Nhập lại password không khớp')
+
+})
+
+export const loginSchema = schema.omit(['confirm_password'])
+export type Schema = yup.InferType<typeof schema>
